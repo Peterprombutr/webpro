52730fe7452dba7412d351c6d73b22490099aa00
Aliapoh9 <anu5200@hotmail.com>
2020-10-13 17:49:29 +0700

    created pages folder and moved LandingPage into it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { LandingPage } from './LandingPage';
+import { LandingPage } from './pages/LandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
