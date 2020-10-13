b64bfb2e92b7f36e394fac11981c8d0256410edc
Aliapoh9 <anu5200@hotmail.com>
2020-10-13 12:35:13 +0700

    edited index to show the landing page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { LandingPage } from './LandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
