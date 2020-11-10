840363c2c00a619e228de2ce89411420b1f83a70
Aliapoh9 <anu5200@hotmail.com>
2020-11-10 21:27:45 +0700

    created LeaderboardItem component

---
+++ src/components/LeaderboardItem/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./style.css";
+
+export class LeaderboardItem extends React.Component {
+
+    render() {
+        return(
+            <div className="leaderboard-item">
+                <p>AmbiguousPamesan&emsp;&emsp;WPM:212&emsp;&emsp;Floor cleared:10</p>
+            </div>
+        );
+    }
+}
