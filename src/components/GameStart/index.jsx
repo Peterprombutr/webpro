d371593b7f682df891fdc443d4c059457d5e123a
Aliapoh9 <anu5200@hotmail.com>
2020-11-12 13:14:25 +0700

    edited difficulty div to handle difficulty change function

--- src/components/GameStart/index.jsx
+++ src/components/GameStart/index.jsx
@@ -10,11 +10,11 @@ export class GameStart extends React.Component {
         };
     }
 
-    change_difficulty = (val) => {
+    change_difficulty = (event) => {
         this.setState({
-            selected: val
+            selected: event.target.value
         });
-        console.log("difficulty changed to " + val);
+        console.log("difficulty changed to " + event.target.value);
     }
 
     render() {
@@ -22,13 +22,13 @@ export class GameStart extends React.Component {
             <div className="dc">
                 <p className="header__difficulty">Select Difficulty</p>
                 <div className="flex">
-                    <div className="difficulty">
+                    <div className="difficulty" onChange={this.change_difficulty}>
                         <input type="radio" id="easy" name="difficulty" value="1" />
-                        <label for="easy">Easy</label>
+                        <label htmlFor="easy">Easy</label>
                         <input type="radio" id="medium" name="difficulty" value="2" />
-                        <label for="medium">Intermediate</label>
+                        <label htmlFor="medium">Intermediate</label>
                         <input type="radio" id="hard" name="difficulty" value="3" />
-                        <label for="hard">Advanced</label>
+                        <label htmlFor="hard">Advanced</label>
                     </div>
                     <div>
                         <button className="button game-start" onClick={() => this.props.start(this.state.selected)}>START</button>
