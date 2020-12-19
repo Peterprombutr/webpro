09f5fa3ac0209c69bce5210d35c5883c58f327fc
Aliapoh9 <anu5200@hotmail.com>
2020-12-19 21:47:33 +0700

    created WordBank component

--- src/components/WordBank/index.jsx
+++ src/components/WordBank/index.jsx
@@ -10,10 +10,26 @@ export class WordBank extends React.Component {
         }
     }
 
+    buildWordBank() {
+        // To integrate with backend later
+        var wordBank = ["conspire", "misuse", "spiteful", "neat", "road", "idiotic", "implant",
+            "muscle", "tin", "finger", "needy", "arrest", "chance", "blue-eyed", "tie", "quiver",
+            "love", "trip", "form"];
+        var wordBankHtml = [];
+
+        for (var i = 0; i < wordBank.length; i++) {
+            wordBankHtml.push(<p key={i} className="word">{wordBank[i]}</p>);
+        }
+
+        return wordBankHtml;
+    }
+
     render() {
+        var wb = this.buildWordBank();
+
         return(
             <div className="word-bank">
-                <p>Conspire misuse spitefuul neat</p>
+                {wb}
             </div>
         );
     }
