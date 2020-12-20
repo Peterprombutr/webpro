7341327951f47fcfee10636ec02400201dd2d0d9
Aliapoh9 <anu5200@hotmail.com>
2020-12-20 18:00:00 +0700

    created TypingArea component

---
+++ src/components/TypingArea/index.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import "./style.css";
+
+export class TypingArea extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            a: 0
+        };
+    }
+
+    render() {
+        return(
+            <div className="typing-area">
+                <input className="typing-area__input"/>
+            </div>
+        );
+    }
+}
