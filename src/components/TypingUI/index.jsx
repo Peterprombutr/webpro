c14a56a4f72e420eceee6e030107d1328d71dffa
Aliapoh9 <anu5200@hotmail.com>
2020-12-22 02:28:29 +0700

    added wpm calculation

--- src/components/TypingUI/index.jsx
+++ src/components/TypingUI/index.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { PlayerInfo } from "../PlayerInfo";
 import { TypingArea } from "../TypingArea";
 import { WordBank } from "../WordBank";
+import { currentTime } from "../../utils/currentTime";
 import "./style.css";
 
 export class TypingUI extends React.Component {
@@ -9,9 +10,12 @@ export class TypingUI extends React.Component {
         super(prop);
 
         this.state = {
+            start_time: false,
+            num_words_typed: 0,
+            wpm: 0,
             current_word: -1,
             typed_words: [],
-            word_bank: []
+            word_bank: [],
         }
     }
 
@@ -24,17 +28,27 @@ export class TypingUI extends React.Component {
     }
 
     type(e) {
+        if (!this.state.start_time) {
+            this.setState({
+                start_time: currentTime()
+            })
+        }
         var w = this.state.word_bank[this.state.current_word]
+        //console.log(this.state.start_time);
 
         if (e.target.value === w + " ") {
            // console.log("hit");
             var updated_typed_words = this.state.typed_words.concat(w);
+            var duration = (currentTime() - this.state.start_time) / 60000.0;
             e.target.value = "";
 
             this.setState({
                 current_word: this.state.current_word + 1,
-                typed_words: updated_typed_words
+                typed_words: updated_typed_words,
+                num_words_typed: this.state.num_words_typed + 1,
+                wpm: ((this.state.num_words_typed + 1) / duration).toFixed(2)
             })
+
         }
     }
 
@@ -74,7 +88,7 @@ export class TypingUI extends React.Component {
             <div className="typing-ui">
                 <WordBank wb={wb}/>
                 <TypingArea onChange={(e) => this.type(e)}/>
-                <PlayerInfo />
+                <PlayerInfo wpm={this.state.wpm}/>
             </div>
         );
     }
