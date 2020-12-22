a086628fa50b0cbb3cca127c5d8fb70218c23b8c
Aliapoh9 <anu5200@hotmail.com>
2020-12-22 20:33:41 +0700

    added main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { BrowserRouter as Router,Switch, Route, Link } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import { TypingPage } from "./pages/TypingPage";
+
+import "./styles/main.css";
+
+export class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            difficulty: 0
+        };
+    }
+
+    start_game = (val) => {
+        this.setState({
+            difficulty: val
+        });
+        console.log("starting a game session with the difficulty of " + val);
+    }
+
+    render() {
+
+        return(
+            <div>
+                <Router>
+                    <Switch>
+                        <Route path="/typing" render={(props) => <TypingPage {...props} difficulty={this.state.difficulty} />} />
+                        <Route path="/" render={(props) => <LandingPage {...props} start_game={this.start_game} />} />
+                    </Switch>
+                </ Router>
+            </div>
+        );
+    }
+}
