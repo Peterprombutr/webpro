24125f460b9fc76cb8677e00efcd694ed06384d8
Aliapoh9 <anu5200@hotmail.com>
2020-12-24 03:09:52 +0700

    removed Link from import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router,Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { TypingPage } from "./pages/TypingPage";
 
