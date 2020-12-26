80574f362847b5e56c6851b3ecc6832469114e8a
Aliapoh9 <anu5200@hotmail.com>
2020-12-27 00:13:17 +0700

    added speed scaling to enemyattack

--- src/components/EnemyAttack/index.jsx
+++ src/components/EnemyAttack/index.jsx
@@ -11,11 +11,16 @@ export class EnemyAttack extends React.Component {
             attack_charge: 0,
             charge_width: 0,
             top: "25vh",
+            base_speed: 5,
             speed: 5,
         }
     }
 
     componentDidMount() {
+        let scaled_speed = (1 + this.props.floor / 20) * this.state.base_speed * this.props.difficulty;
+        this.setState({
+            speed: scaled_speed
+        })
         this.intervalID = setInterval(() => this.incrementCharge(this.state.attack_charge, this.state.speed), 500);
         this.intervalID2 = setInterval(() => this.restartAttack(), attack_restart_timer);
     }
@@ -26,7 +31,7 @@ export class EnemyAttack extends React.Component {
     }
 
     incrementCharge(charge, speed) {
-
+        console.log(speed);
         if (this.state.attack_charge >= 100) {
             this.props.take_damage();
             this.deactivate_attack();
@@ -52,8 +57,10 @@ export class EnemyAttack extends React.Component {
             this.props.attack_commence();
             this.intervalID = setInterval(() => this.incrementCharge(this.state.attack_charge, this.state.speed), 500);
             var new_top = Math.floor(8 + Math.random() * 40) + "vh";
+            let scaled_speed = (1 + this.props.floor / 20) * this.state.base_speed * this.props.difficulty;
             this.setState({
-                top: new_top
+                top: new_top,
+                speed: scaled_speed
             })
         }
 
