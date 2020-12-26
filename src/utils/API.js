e7abd06d16a30d3b424219fa233140a1cbfd1d3a
Aliapoh9 <anu5200@hotmail.com>
2020-12-27 01:57:19 +0700

    added cancel signal to api

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,16 +4,17 @@ axios.defaults.baseURL = 'https://typeit-backend.et.r.appspot.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export class API {
+    static signal = axios.CancelToken.source();
 
     static async getWordBank(n) {
-        var promise = axios.get("/api/wordbank", { params: { wb_name:"common200", num:n }});
+        var promise = axios.get("/api/wordbank", { params: { wb_name:"common200", num:n, cancelToken: this.signal.token}});
         var dataPromise = promise.then(response => response.data)
 
         return dataPromise;
     }
 
     static async getMonster(id) {
-        var promise = axios.get("/api/monster", { params: { m_id:id }});
+        var promise = axios.get("/api/monster", { params: { m_id:id , cancelToken: this.signal.token}});
         var dataPromise = promise.then(response => response.data)
 
         return dataPromise;
