1dbeacaf33bb20c1155c79dff121bc8b7cc5d17d
Aliapoh9 <anu5200@hotmail.com>
2020-12-27 01:57:28 +0700

    add GameOverPage

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { TypingPage } from "./pages/TypingPage";
+import { GameOverPage } from "./pages/GameOverPage";
 import SignIn from "./components/LoginComponent/SignIn";
 import SignUp from "./components/LoginComponent/SignUp";
 
@@ -25,9 +26,15 @@ export class Main extends React.Component {
     }
 
     login = (displayName) => {
-        console.log("logged in: " + displayName);
         this.setState({
             username: displayName
+        })
+    }
+
+    logout = () => {
+        console.log("logout");
+        this.setState({
+            username: null
         })
     }
 
@@ -39,8 +46,9 @@ export class Main extends React.Component {
                     <Switch>
                         <Route path="/login" render={(props) => <SignIn login={(username) => this.login(username)}/>} />
                         <Route exact path="/signup" render={(props) => <SignUp login={(username) => this.login(username)}/>} />
-                        <Route path="/typing" render={(props) => <TypingPage {...props} difficulty={this.state.difficulty} username={this.state.username}/>} />
-                        <Route path="/" render={(props) => <LandingPage {...props} start_game={this.start_game} username={this.state.username}/>} />
+                        <Route path="/end" render={(props) => <GameOverPage {...props} logout={this.logout} username={this.state.username}/>} />
+                        <Route path="/typing" render={(props) => <TypingPage {...props} difficulty={this.state.difficulty} logout={this.logout} username={this.state.username}/>} />
+                        <Route path="/" render={(props) => <LandingPage {...props} start_game={this.start_game} logout={this.logout} username={this.state.username}/>} />
                     </Switch>
                 </ Router>
             </div>
